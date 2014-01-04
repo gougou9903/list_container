5bde0c320a7c10146c223115c1c185bf803cfc3e
SergNikitin <snikitin@outlook.com>
2014-01-04 23:15:50 +0400

    - 'line' element made unsigned - getDescription returned string is not const now #6

--- exception.cpp
+++ exception.cpp
@@ -2,12 +2,13 @@
 #include <string>
 #include "exception.h"
 
-Exception::Exception(std::string _description,
-                     std::string _fileName, int _line) : description(_description),
-                                                         fileName(_fileName),
-                                                         line(_line) {}
+Exception::Exception(std::string _description, std::string _fileName,
+                     unsigned int _line)
+    : description(_description)
+    , fileName(_fileName)
+    , line(_line) {}
 
-const std::string Exception::getDescription() const {
+std::string Exception::getDescription() const {
     std::stringstream errorDesc;
     errorDesc << description << " at line " << line << " of the file " << fileName;
     return errorDesc.str();
--- exception.h
+++ exception.h
@@ -11,15 +11,15 @@ public:
     * @param _filename Name of the file that generated error
     * @param _line Number of the line that generated error
     */
-    Exception(std::string _description, std::string _fileName, int _line);
+    Exception(std::string _description, std::string _fileName, unsigned int _line);
 
     /**
     * @return String with error info, including line number and filename
     */
-    const std::string getDescription() const;
+    std::string getDescription() const;
 
 private:
     std::string description;
     std::string fileName;
-    int line;
+    unsigned int line;
 };
